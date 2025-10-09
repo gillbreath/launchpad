ba97e254345fc3b867412d3ef0baaebb1e8d5319
Elias <elias@eliasjames.com>
2025-10-09 17:00:35 -0400

    basic crud viewer

---
+++ src/app/crud/[table]/page.js
@@ -0,0 +1,17 @@
+import prisma from '@/lib/prisma';
+
+export default async function Page({ params }) {
+const { table } = await params
+console.log('t',  prisma[table] )
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">
+          crud {table}
+        </h1>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
